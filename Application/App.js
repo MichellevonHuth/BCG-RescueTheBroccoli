b5f2dc2e8e371eb3514e5c796fcece4b78081dc1
Michelle von Huth <56582203+MichellevonHuth@users.noreply.github.com>
2022-11-02 08:10:10 +0100

    Add files via upload

---
+++ Application/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MainContainer from './src/navigations/MainNavigation';
+import ShopContainer from './src/navigations/ShopNavigation';
+import UserContainer from './src/navigations/ShopNavigation';
+import IntroScreen from './src/screens/Home/IntroScreen';
+
+export default function App() {
+  return (
+     <MainContainer />
+  );
+}
